1b1f40cf970ffe9097bf994a80fda4e2471561ce
ronnycray <chayka1998A@yandex.ru>
2018-11-09 15:25:05 +0300

    scroll-swipe-down | doc with jquery

---
+++ scripts/swipe-script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $(".start_search").click(function () {
+        var scroll_el = $(this).attr("href"); // получаем ссылку
+        if ($(scroll_el).length != 0) { // проверим существование элемента чтобы избежать ошибки
+            $('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 500); // анимируем скроолинг к элементу scroll_el
+            }
+        return false; // выключаем стандартное действие 
+    });
+});
