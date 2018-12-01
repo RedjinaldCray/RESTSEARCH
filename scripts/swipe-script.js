9c01028d96a70b72feb2a7552129aff4a02c7dfb
ronnycray <chayka1998A@yandex.ru>
2018-12-02 01:47:17 +0300

    add swipe, box, btn-fix | V.0.2

--- scripts/swipe-script.js
+++ scripts/swipe-script.js
@@ -1,8 +1,8 @@
 jQuery(document).ready(function () {
-    $(".start_search").click(function () {
+    $(".start").click(function () {
         var scroll_el = $(this).attr("href"); // получаем ссылку
         if ($(scroll_el).length != 0) { // проверим существование элемента чтобы избежать ошибки
-            $('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 500); // анимируем скроолинг к элементу scroll_el
+            $('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 2000); // анимируем скроолинг к элементу scroll_el
             }
 
         return false; // выключаем стандартное действие 
