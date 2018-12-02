9fc85e9872ba6c1e1983a267ffcf13e70fa86b3a
ronnycray <chayka1998A@yandex.ru>
2018-12-02 10:50:57 +0300

    fix scroll

--- scripts/swipe-script.js
+++ scripts/swipe-script.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function () {
     $(".start").click(function () {
         var scroll_el = $(this).attr("href"); // получаем ссылку
         if ($(scroll_el).length != 0) { // проверим существование элемента чтобы избежать ошибки
-            $('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 2000); // анимируем скроолинг к элементу scroll_el
+            $('html, body').animate({ scrollTop: $(scroll_el).offset().top - 125 }, 1500); // анимируем скроолинг к элементу scroll_el
             }
 
         return false; // выключаем стандартное действие 
