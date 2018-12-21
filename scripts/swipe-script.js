fd298453a30e2b781bc95e1a84af71beedb2ead1
ronnycray <chayka1998A@yandex.ru>
2018-12-21 03:33:04 +0300

    new pages V.1.1

--- scripts/swipe-script.js
+++ scripts/swipe-script.js
@@ -7,4 +7,22 @@ jQuery(document).ready(function () {
 
         return false; // выключаем стандартное действие 
     });
+
+    $(".start-anti").click(function () {
+        var scroll_el = $(this).attr("href"); // получаем ссылку
+        if ($(scroll_el).length != 0) { // проверим существование элемента чтобы избежать ошибки
+            $('html, body').animate({ scrollTop: $(scroll_el).offset().top - 60 }, 1200); // анимируем скроолинг к элементу scroll_el
+            }
+
+        return false; // выключаем стандартное действие 
+    });
+
+    $(".start-hookah").click(function () {
+        var scroll_el = $(this).attr("href"); // получаем ссылку
+        if ($(scroll_el).length != 0) { // проверим существование элемента чтобы избежать ошибки
+            $('html, body').animate({ scrollTop: $(scroll_el).offset().top - 60 }, 1200); // анимируем скроолинг к элементу scroll_el
+            }
+
+        return false; // выключаем стандартное действие 
+    });
 });
